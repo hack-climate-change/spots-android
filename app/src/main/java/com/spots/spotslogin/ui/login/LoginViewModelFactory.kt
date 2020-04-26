53b701d6c48a027eefd2ac3c6836669aba4991bb
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2020-04-26 15:45:21 +0200

    first commit

---
+++ app/src/main/java/com/spots/spotslogin/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.spots.spotslogin.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.spots.spotslogin.data.LoginDataSource
+import com.spots.spotslogin.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                    loginRepository = LoginRepository(
+                            dataSource = LoginDataSource()
+                    )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
